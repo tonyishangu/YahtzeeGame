ec67d476a3cc2ed77f812717317dfa0e79a78a8a
tonyishangu <tonyishangu@gmail.com>
2023-02-06 14:21:58 +0300

    function to lock the dice you want

--- src/Die.js
+++ src/Die.js
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  constructor(props){
+    super(props)
+    this.handleClicked = this.handleClicked.bind(this)
+  }
+  handleClicked() {
+    this.props.handleClick(this.props.idx)
+  }
   render() {
     return (
       <button
         className={"Die"}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
+        onClick={this.handleClicked}
       >
         {this.props.val}
       </button>
